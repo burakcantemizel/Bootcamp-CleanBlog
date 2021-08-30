d82fb7326cf6dfd0835de3adc770c163a19dcd11
burakcantemizel <burak_838@hotmail.com>
2021-08-30 18:12:45 +0300

    Partial kısımlar oluşturuldu. Routelar ayarlandı.

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const path = require('path');
 const express = require('express');
+const ejs = require('ejs');
 
 const app = express();
+
+// Template Engine Configuration
+app.set('view engine', 'ejs');
+
+// Middlewares
 app.use(express.static('public'));
 
+// Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index')
+});
+
+app.get('/about', (req, res) => {
+  res.render('about')
+});
+
+app.get('/addPost', (req, res) => {
+  res.render('add_post')
 });
 
 const port = 3000;
