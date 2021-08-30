973ae987996014e644a2cf3c146015948ba36aef
burakcantemizel <burak_838@hotmail.com>
2021-08-30 17:10:02 +0300

    statik dosyalar için middleware oluşturuldu.

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+const path = require('path');
 const express = require('express');
+
 const app = express();
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-
-  res.send(blog);
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
 
 const port = 3000;
